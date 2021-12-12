dcfcc835be87cade90fd260dc3dbf202b394f166
mitty1293 <k.fmichi@gmai.com>
2021-12-13 03:02:23 +0900

    fix

--- flask/gunicorn.conf.py
+++ flask/gunicorn.conf.py
@@ -1,2 +1,2 @@
 accesslog = "/var/log/gunicorn_access.log"
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({host}i)s" "%({location}o)s"'
+access_log_format = '%(t)s "\nremote_addr: %(h)s" "\n#request: %(r)s" "\nstatus: %(s)s" "\nhost: %({host}i)s" "\nreferer: %(f)s" "\nlocation: %({location}o)s"'
